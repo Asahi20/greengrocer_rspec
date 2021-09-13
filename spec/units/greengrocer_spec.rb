2163188fa03848b3e6db4fcffd20a7b2f832606f
Asahi20 <asahiyamaguchi1120@gmail.com>
2021-09-13 23:01:05 +0900

    greengrocer_rspec/RSpecの勉強

---
+++ spec/units/greengrocer_spec.rb
@@ -0,0 +1,112 @@
+RSpec.describe Greengrocer do
+  let(:product_params) do
+    [
+      { name: "トマト", price: 100 },
+      { name: "きゅうり", price: 200 }
+    ]
+  end
+  let(:greengrocer) { Greengrocer.new(product_params) }
+  # ▼単体テスト2 正常系（Greengrocerクラスのインスタンス生成）
+  describe ".initialize" do
+    context "インスタンスが生成されたとき" do
+
+        let(:products) { greengrocer.products }
+      it "@productsの要素の数が，product_paramsの要素の数と等しいこと" do
+        expect(products.size).to eq 2
+      end
+
+      it "@productsの最初の要素の名前が，product_paramsの最初の要素の名前と等しいこと" do
+        expect(products[0].name).to eq "トマト"
+      end
+
+      it "@productsの最初の要素の金額が，product_paramsの最初の要素の金額と等しいこと" do
+        expect(products[0].price).to eq 100
+      end
+    end
+  end
+  # ▼単体テスト３ 正常系(register_productメソッド)
+  describe ".register_product" do
+    let(:products){ greengrocer.products }
+    let(:adding_product_params) do
+      [
+        { name: "ごぼう", price: 250 },
+        { name: "れんこん", price: 350 }
+      ]
+    end
+    before { greengrocer.register_product(adding_product_params) }
+    it "@productsの要素の数が、「product_paramasとadding_product_paramsの要素の数の和」と等しいこと" do
+      expect(products.size).to eq 4
+    end
+    it "@productsの最後の要素の名前が，adding_product_paramsの最後の要素の名前と等しいこと" do
+      expect(products[-1].name).to eq "れんこん"
+    end
+    it "@productsの最後の要素の金額が，adding_product_paramsの最後の要素の金額と等しいこと" do
+      expect(products[-1].price).to eq 350
+    end
+  end
+  describe ".disp_products" do
+    let(:base_id) { Product.class_variable_get("@@count") }
+    let(:hello_msg) { "いらっしゃいませ！商品を選んで下さい。" }
+    let(:product_msg1) { "#{base_id + 1}.トマト(¥100)" }
+    let(:product_msg2) { "#{base_id + 2}.きゅうり(¥200)" }
+    let(:msg) { "#{hello_msg}\n#{product_msg1}\n#{product_msg2}\n" }
+    it "期待する表示がされること" do
+      expect{ greengrocer.disp_products }.to output(msg).to_stdout
+    end
+  end
+  describe ".ask_quantity" do
+    let(:chosen_product) { Product.new({ name: "玉ねぎ", price: 300 }) }
+    let(:ask_msg){ "玉ねぎですね。何個買いますか？\n" }
+    it "ussrが選択した商品の名前を含む，期待する表示がされること" do
+      expect { greengrocer.ask_quantity(chosen_product) }.to output(ask_msg).to_stdout
+    end
+  end
+  describe ".calculate_charges" do
+    let(:user) { User.new }
+    let(:thank_msg) { "お買い上げありがとうございました！" }
+
+    shared_examples "正しい合計金額を含む，期待する表示がされること" do
+      it "正しい合計金額を含む，期待する表示がされること" do
+        user.instance_variable_set("@chosen_product", chosen_product)
+        user.instance_variable_set("@quantity_of_product", quantity_of_product)
+        expect { greengrocer.calculate_charges(user) }
+          .to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
+      end
+    end
+    shared_examples "割引した正しい合計金額を含む，期待する表示がされること" do
+      it do
+        user.instance_variable_set("@chosen_product", chosen_product)
+        user.instance_variable_set("@quantity_of_product", quantity_of_product)
+        expect { greengrocer.calculate_charges(user) }
+          .to output("#{discount_msg}\n#{discount_total_price_msg}\n#{thank_msg}\n").to_stdout
+      end
+    end
+
+    context "300円の商品（玉ねぎ）が4個のとき" do
+      let(:chosen_product) { Product.new({ name: "玉ねぎ", price: 300 }) }
+      let(:quantity_of_product) { 4 }
+      let(:total_price_msg) { "合計金額は1200円です。" }
+      it_behaves_like "正しい合計金額を含む，期待する表示がされること"
+    end
+    context "400円の商品（なす）が4個のとき" do
+      let(:chosen_product) { Product.new({ name: "なす", price: 400 }) }
+      let(:quantity_of_product) { 4 }
+      let(:total_price_msg) { "合計金額は1600円です。" }
+      it_behaves_like "正しい合計金額を含む，期待する表示がされること"
+    end
+    context "300円の商品（玉ねぎ）が5個のとき" do
+      let(:chosen_product) { Product.new({ name: "玉ねぎ", price: 300 }) }
+      let(:quantity_of_product) { 5 }
+      let(:discount_msg) { "5個以上なので10％割引となります！" }
+      let(:discount_total_price_msg) { "合計金額は1350円です。" }
+      it_behaves_like "割引した正しい合計金額を含む，期待する表示がされること"
+    end
+    context "400円の商品（なす）が5個のとき" do
+      let(:chosen_product) { Product.new({ name: "なす", price: 400 }) }
+      let(:quantity_of_product) { 5 }
+      let(:discount_msg) { "5個以上なので10％割引となります！" }
+      let(:discount_total_price_msg) { "合計金額は1800円です。" }
+      it_behaves_like "割引した正しい合計金額を含む，期待する表示がされること"
+    end
+  end
+end
---
+++ spec/units/user_spec.rb
@@ -0,0 +1,89 @@
+RSpec.describe User do
+  let(:user) { User.new }
+  describe ".choose_product" do
+    let(:products) do
+      [
+        Product.new({ name: "トマト", price: 100 }),
+        Product.new({ name: "きゅうり", price: 200 })
+      ]
+    end
+    let(:correct_product_id_input) { "#{products.first.id}\n" }
+
+    context "存在するid（productsの最初の要素のid）を入力したとき" do
+      before do
+        allow(ARGF).to receive(:gets).and_return correct_product_id_input
+        user.choose_product(products)
+      end
+      it "@chosen_productのidが，productsの最初の要素のidと等しいこと" do
+        expect(user.chosen_product.id).to eq correct_product_id_input.to_i
+      end
+      it "@chosen_productの名前が，productsの最初の要素の名前と等しいこと" do
+        expect(user.chosen_product.name).to eq "トマト"
+      end
+      it "@chosen_productの金額が，productsの最初の要素の金額と等しいこと" do
+        expect(user.chosen_product.price).to eq 100
+      end
+    end
+
+    let(:prompt_re_enter_msg) { /#{products.first.id}から#{products.last.id}の番号から選んでください。\n/ }
+
+    shared_examples "再入力を促すこと" do
+      it do
+        allow(ARGF).to receive(:gets).and_return wrong_product_id_input, correct_product_id_input
+        expect { user.choose_product(products) }.to output(prompt_re_enter_msg).to_stdout
+      end
+    end
+    context "商品一覧の最初のidより１小さい数値を入力したとき" do
+      let(:wrong_product_id_input) { "#{products.first.id - 1}\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+    context "商品一覧の最後のidより１大きい数値を入力したとき" do
+      let(:wrong_product_id_input) { "#{products.last.id + 1}\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+    context "数値以外の文字列を入力したとき" do
+      let(:wrong_product_id_input) { "hoge\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+  end
+  describe ".decide_quantity" do
+    shared_examples "@quantify_of_productが，入力値を整数化した値と等しいこと" do
+      it do
+        allow(ARGF).to receive(:gets).and_return correct_quantify_input
+        user.decide_quantity
+        expect(user.quantity_of_product).to eq correct_quantify_input.to_i
+      end
+    end
+    context "1を入力したとき" do
+      let(:correct_quantify_input) { "1\n" }
+      it_behaves_like "@quantify_of_productが，入力値を整数化した値と等しいこと"
+    end
+    context "2~100の数値のいずれかを入力したとき" do
+      let(:correct_quantify_input) { "#{rand(2..100)}\n" }
+      it_behaves_like "@quantify_of_productが，入力値を整数化した値と等しいこと"
+    end
+
+    let(:prompt_re_enter_msg) { /１個以上選んでください。/ }
+    shared_examples "再入力を促すこと" do
+      it  do
+        allow(ARGF).to receive(:gets).and_return wrong_quantify_input, correct_quantify_input
+        expect{ user.decide_quantity }.to output(prompt_re_enter_msg).to_stdout
+      end
+    end
+    context "0を入力したとき" do
+      let(:wrong_quantify_input) { "0\n" }
+      let(:correct_quantify_input) { "1\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+    context "負の整数を入力したとき" do
+      let(:wrong_quantify_input) { "#{rand(-100..-1)}\n" }
+      let(:correct_quantify_input) { "1\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+    context "数値以外の文字列を入力したとき" do
+      let(:wrong_quantify_input) { "hoge\n" }
+      let(:correct_quantify_input) { "1\n" }
+      it_behaves_like "再入力を促すこと"
+    end
+  end
+end
